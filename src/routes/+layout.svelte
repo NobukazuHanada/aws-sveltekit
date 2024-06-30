7ee8c0ef956a5dcf91ee362fba986a60d33e6b48
Nobkz <tone.nobukazu@gmail.com>
2024-06-30 12:27:23 +0900

    move configure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,1 @@
-<script lang="ts">
-	import { Amplify, type ResourcesConfig } from 'aws-amplify';
-	import { env } from '$env/dynamic/public';
-
-	Amplify.configure({
-		Auth: {
-			Cognito: {
-				userPoolClientId: env.PUBLIC_AUTH_USER_CLIENDT_ID!,
-				userPoolId: env.PUBLIC_AUTH_USER_POOL_ID!
-			}
-		}
-	});
-</script>
-
 <slot></slot>
---
+++ src/routes/+layout.ts
@@ -0,0 +1,11 @@
+import { Amplify, type ResourcesConfig } from 'aws-amplify';
+import { env } from '$env/dynamic/public';
+
+Amplify.configure({
+	Auth: {
+		Cognito: {
+			userPoolClientId: env.PUBLIC_AUTH_USER_CLIENDT_ID!,
+			userPoolId: env.PUBLIC_AUTH_USER_POOL_ID!
+		}
+	}
+});
