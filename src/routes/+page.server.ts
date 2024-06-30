bca94db0e1f8b04437c9be7f6bcf75511f354406
Nobkz <tone.nobukazu@gmail.com>
2024-06-30 11:20:14 +0900

    add logger

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,29 +1,40 @@
+import { logger } from '$lib/logger.js';
+import { fail } from '@sveltejs/kit';
 import { signIn, fetchAuthSession } from 'aws-amplify/auth';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ cookies, request }) => {
+		logger.info('start signin');
 		const data = await request.formData();
 		const username = data.get('username') as string;
 		const password = data.get('password') as string;
+		logger.info({ username, password }, 'sign in data');
 
-		const { nextStep } = await signIn({
-			username,
-			password,
-			options: {
-				authFlowType: 'USER_SRP_AUTH'
+		try {
+			const { nextStep } = await signIn({
+				username,
+				password,
+				options: {
+					authFlowType: 'USER_SRP_AUTH'
+				}
+			});
+			const session = await fetchAuthSession();
+			const token = session.tokens?.idToken?.toString();
+			if (token) {
+				cookies.set('token', token, { path: '/' });
 			}
-		});
-		const session = await fetchAuthSession();
-		const token = session.tokens?.idToken?.toString();
-		if (token) {
-			cookies.set('token', token, { path: '/' });
-		}
-		if (nextStep.signInStep === 'CONTINUE_SIGN_IN_WITH_TOTP_SETUP') {
-			const secretCode = nextStep.totpSetupDetails.sharedSecret;
-			return { signInStep: nextStep.signInStep, token, secretCode };
-		}
+			logger.info({ nextStep, session }, 'sign in success');
 
-		return { signInStep: nextStep.signInStep, token };
+			if (nextStep.signInStep === 'CONTINUE_SIGN_IN_WITH_TOTP_SETUP') {
+				const secretCode = nextStep.totpSetupDetails.sharedSecret;
+				return { signInStep: nextStep.signInStep, token, secretCode };
+			}
+
+			return { signInStep: nextStep.signInStep, token };
+		} catch (e) {
+			logger.error(e, 'signin error occurred');
+			fail(400, { e });
+		}
 	}
 };
